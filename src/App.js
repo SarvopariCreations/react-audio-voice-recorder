e95e2e9098b94ec28a5033ca534f9ffab8602850
SarvopariCreations <146719258+SarvopariCreations@users.noreply.github.com>
2024-04-22 16:00:34 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import React from "react";
+
+import ReactDOM from "react-dom/client";
+import { AudioRecorder } from 'react-audio-voice-recorder';
+function App() {
+
+  const addAudioElement = (blob) => {
+    const url = URL.createObjectURL(blob);
+    const audio = document.createElement("audio");
+    audio.src = url;
+    audio.controls = true;
+    document.body.appendChild(audio);
+  };
+  
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <AudioRecorder 
+        onRecordingComplete={addAudioElement}
+        audioTrackConstraints={{
+          noiseSuppression: true,
+          echoCancellation: true,
+        }} 
+        downloadOnSavePress={true}
+        downloadFileExtension="webm"
+      />
+    </React.StrictMode>
+  ); 
+}
+export default App;
